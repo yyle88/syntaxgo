a021de73f41d4fece436cd07f7a4b4cf2366e7cb
yangyile <example@gogit.com>
2024-05-19 21:45:58 +0800

    添加测试代码

--- syntaxgo_test.go
+++ syntaxgo_test.go
@@ -1,7 +1,16 @@
-package syntaxgo
+package syntaxgo_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/yyle88/runpath/runtestpath"
+	"github.com/yyle88/syntaxgo"
+)
 
 func TestCurrentPackageName(t *testing.T) {
-	t.Log(CurrentPackageName())
+	t.Log(syntaxgo.CurrentPackageName())
+}
+
+func TestGetPkgName(t *testing.T) {
+	t.Log(syntaxgo.GetPkgName(runtestpath.SrcName(t)))
 }
