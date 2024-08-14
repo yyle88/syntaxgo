4895ffba9f4a5084b38c2cef77fee4b85e5394df
yangyile <example@gogit.com>
2024-08-14 15:36:24 +0700

    添加逻辑提取出标签内的键值字段

--- syntaxgo_tag/extract.go
+++ syntaxgo_tag/extract.go
@@ -3,6 +3,7 @@ package syntaxgo_tag
 import (
 	"fmt"
 	"regexp"
+	"strings"
 
 	"github.com/pkg/errors"
 )
@@ -13,7 +14,7 @@ func ExtractTagValue(tag, key string) string {
 	// \b 确保匹配整个单词，避免部分匹配
 	// \s 空白
 	// 该正则的小括号没有把空白括起来，因此结果不包含空白，即不包含空格部分
-	regex := regexp.MustCompile(`\b` + key + `\s*:\s*"([^"]*)"`)
+	regex := regexp.MustCompile(`\b` + regexp.QuoteMeta(key) + `\s*:\s*"([^"]*)"`)
 	if matches := regex.FindStringSubmatch(tag); len(matches) > 1 {
 		return matches[1]
 	}
@@ -37,9 +38,9 @@ func ExtractTagField(part, fieldName string, action ExtractTagFieldAction) strin
 	// 认为存在需要完整匹配 和 排除前导空格 两种需求，通常在写标签内部的k:v时不应该添加前导空格
 	var regex *regexp.Regexp
 	if action == EXCLUDE_WHITESPACE_PREFIX {
-		regex = regexp.MustCompile(`\b` + fieldName + `\s*:\s*([^;]+)`)
+		regex = regexp.MustCompile(`\b` + regexp.QuoteMeta(fieldName) + `\s*:\s*([^;]+)`)
 	} else if action == INCLUDE_WHITESPACE_PREFIX {
-		regex = regexp.MustCompile(`\b` + fieldName + `\s*:([^;]+)`)
+		regex = regexp.MustCompile(`\b` + regexp.QuoteMeta(fieldName) + `\s*:([^;]+)`)
 	} else {
 		panic(errors.New("WRONG"))
 	}
@@ -51,7 +52,7 @@ func ExtractTagField(part, fieldName string, action ExtractTagFieldAction) strin
 
 func ExtractTagValueIndex(tag, key string) (string, int, int) {
 	// 正则表达式查找键值对
-	regex := regexp.MustCompile(`\b` + key + `\s*:\s*"([^"]*)"`)
+	regex := regexp.MustCompile(`\b` + regexp.QuoteMeta(key) + `\s*:\s*"([^"]*)"`)
 	// 查找键值对及其位置
 	if indexes := regex.FindStringSubmatchIndex(tag); len(indexes) > 3 {
 		sdx := indexes[2]
@@ -66,9 +67,9 @@ func ExtractTagFieldIndex(part, fieldName string, action ExtractTagFieldAction) 
 	// 正则表达式查找键值对
 	var regex *regexp.Regexp
 	if action == EXCLUDE_WHITESPACE_PREFIX {
-		regex = regexp.MustCompile(`\b` + fieldName + `\s*:\s*([^;]+)`)
+		regex = regexp.MustCompile(`\b` + regexp.QuoteMeta(fieldName) + `\s*:\s*([^;]+)`)
 	} else if action == INCLUDE_WHITESPACE_PREFIX {
-		regex = regexp.MustCompile(`\b` + fieldName + `\s*:([^;]+)`)
+		regex = regexp.MustCompile(`\b` + regexp.QuoteMeta(fieldName) + `\s*:([^;]+)`)
 	} else {
 		panic(errors.New("WRONG"))
 	}
@@ -81,8 +82,41 @@ func ExtractTagFieldIndex(part, fieldName string, action ExtractTagFieldAction) 
 	return "", -1, -1
 }
 
-func ExtractNoValueTagFieldNameIndex(part, fieldName string) (sdx, edx int) {
-	re := regexp.MustCompile(fmt.Sprintf(`\b(%s)\b(?:;|$)`, fieldName))
+// ExtractNoValueFieldNameIndex 匹配单键标签，比如 index 或者 uniqueIndex 这类标签，在简化情况下可以是没有值的
+// 返回的是键名的起止坐标，区间包含键名左右的空格部分
+// 目前下游使用这个函数的目的是提取到键名的位置，再往里面补充东西
+func ExtractNoValueFieldNameIndex(part, fieldName string) (sdx, edx int) {
+	// 保留两个 \b，并在 fieldName 和分号/字符串结尾之间允许空格
+	re := regexp.MustCompile(fmt.Sprintf(`(\s*\b%s\b\s*)(?:;|$)`, regexp.QuoteMeta(fieldName)))
+
+	// 查找匹配的位置
+	matches := re.FindStringSubmatchIndex(part)
+	if len(matches) > 3 {
+		return matches[2], matches[3]
+	}
+	return -1, -1
+}
+
+// ExtractFieldEqualsValueIndex 返回的是键值对中值的坐标 比如匹配的是 index:idx_abc 或者 uniqueIndex:udx_xyz 这种键值对
+// 返回的是值的起止坐标，同时，区间内包含前后的空格
+// 目前下游使用这个函数的目的是，获取到值的坐标，替换值的内容
+func ExtractFieldEqualsValueIndex(part, fieldName, fieldValue string) (sdx, edx int) {
+	return ExtractFieldEqualsValueIndexV2(part, fieldName, fieldValue, []string{})
+}
+
+// ExtractFieldEqualsValueIndexV2 和前面的功能相同，但提供自定义分隔符的功能
+func ExtractFieldEqualsValueIndexV2(part string, fieldName string, fieldValue string, terminators []string) (int, int) {
+	//首先确保有一个分隔符是分号，这样将来和 $ 或的时候就没有语法错误
+	var mergeTerminators = []string{";"}
+	for _, v := range terminators {
+		//接着把传进来的都进行安全处理
+		mergeTerminators = append(mergeTerminators, regexp.QuoteMeta(v))
+	}
+	// 最后把它们以或连接起来，当然其内容有可能就只是";"这个字符串，表示没有额外的分隔符
+	ts := strings.Join(mergeTerminators, "|")
+
+	// 构建正则表达式，匹配 fieldName 和 fieldValue，并确保它们两边的空格被忽略
+	re := regexp.MustCompile(fmt.Sprintf(`\s*\b%s\b\s*:(\s*%s\s*)(?:`+ts+`|$)`, regexp.QuoteMeta(fieldName), regexp.QuoteMeta(fieldValue)))
 
 	// 查找匹配的位置
 	matches := re.FindStringSubmatchIndex(part)
--- syntaxgo_tag/extract_test.go
+++ syntaxgo_tag/extract_test.go
@@ -72,82 +72,170 @@ func TestExtractTagFieldIndex_2(t *testing.T) {
 	require.Equal(t, field, sub)
 }
 
-func TestExtractNoValueTagFieldNameIndex(t *testing.T) {
+func TestExtractNoValueFieldNameIndex(t *testing.T) {
 	const tmp = "column:name;index;"
 
-	sdx, edx := ExtractNoValueTagFieldNameIndex(tmp, "index")
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "index")
 	t.Log(sdx, edx)
 	require.Equal(t, "index", tmp[sdx:edx])
 }
 
-func TestExtractNoValueTagFieldNameIndex_2(t *testing.T) {
+func TestExtractNoValueFieldNameIndex_2(t *testing.T) {
 	const tmp = "column:name;index:abc;"
 
-	sdx, edx := ExtractNoValueTagFieldNameIndex(tmp, "index")
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "index")
 	require.Equal(t, -1, sdx)
 	require.Equal(t, -1, edx)
 }
 
-func TestExtractNoValueTagFieldNameIndex_3(t *testing.T) {
+func TestExtractNoValueFieldNameIndex_3(t *testing.T) {
 	const tmp = "column:name;index;field2"
 
-	sdx, edx := ExtractNoValueTagFieldNameIndex(tmp, "index")
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "index")
 	t.Log(sdx, edx)
 	require.Equal(t, "index", tmp[sdx:edx])
 }
 
-func TestExtractNoValueTagFieldNameIndex_4(t *testing.T) {
+func TestExtractNoValueFieldNameIndex_4(t *testing.T) {
 	const tmp = "column:name;index:value;field2"
 
-	sdx, edx := ExtractNoValueTagFieldNameIndex(tmp, "index")
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "index")
 	require.Equal(t, -1, sdx)
 	require.Equal(t, -1, edx)
 }
 
-func TestExtractNoValueTagFieldNameIndex_5(t *testing.T) {
+func TestExtractNoValueFieldNameIndex_5(t *testing.T) {
 	const tmp = "column:name; index;field2"
 
-	sdx, edx := ExtractNoValueTagFieldNameIndex(tmp, "index")
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "index")
+	t.Log(sdx, edx)
+	require.Equal(t, " index", tmp[sdx:edx])
+}
+
+func TestExtractNoValueFieldNameIndex_6(t *testing.T) {
+	const tmp = "column:name;index;field2"
+
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "field2")
+	t.Log(sdx, edx)
+	require.Equal(t, "field2", tmp[sdx:edx])
+}
+
+func TestExtractNoValueFieldNameIndex_7(t *testing.T) {
+	const tmp = "column:name;index;field2;"
+
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "field2")
+	t.Log(sdx, edx)
+	require.Equal(t, "field2", tmp[sdx:edx])
+}
+
+func TestExtractNoValueFieldNameIndex_8(t *testing.T) {
+	const tmp = "column:name;index;field2:value;"
+
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "field2")
+	require.Equal(t, -1, sdx)
+	require.Equal(t, -1, edx)
+}
+
+func TestExtractNoValueFieldNameIndex_9(t *testing.T) {
+	const tmp = "column:name;index;field2: value;"
+
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "field2")
+	require.Equal(t, -1, sdx)
+	require.Equal(t, -1, edx)
+}
+
+func TestExtractNoValueFieldNameIndex_10(t *testing.T) {
+	const tmp = "column:name;index :value;field2"
+
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "index")
+	require.Equal(t, -1, sdx)
+	require.Equal(t, -1, edx)
+}
+
+func TestExtractNoValueFieldNameIndex_11(t *testing.T) {
+	const tmp = "column:name;index;field2 ;"
+
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "field2")
+	t.Log(sdx, edx)
+	require.Equal(t, "field2 ", tmp[sdx:edx])
+}
+
+func TestExtractNoValueFieldNameIndex_12(t *testing.T) {
+	const tmp = "column:name;index ;field2"
+
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "index")
+	t.Log(sdx, edx)
+	require.Equal(t, "index ", tmp[sdx:edx])
+}
+
+func TestExtractNoValueFieldNameIndex_13(t *testing.T) {
+	const tmp = "column:name;  index  ;field2"
+
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "index")
+	t.Log(sdx, edx)
+	require.Equal(t, "  index  ", tmp[sdx:edx])
+}
+
+func TestExtractNoValueFieldNameIndex_14(t *testing.T) {
+	const tmp = "column:name;index;field2:value"
+
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "index")
 	t.Log(sdx, edx)
 	require.Equal(t, "index", tmp[sdx:edx])
 }
 
-func TestExtractNoValueTagFieldNameIndex_6(t *testing.T) {
-	const tmp = "column:name;index;field2"
+func TestExtractNoValueFieldNameIndex_15(t *testing.T) {
+	const tmp = "column:name;index;field2 "
 
-	sdx, edx := ExtractNoValueTagFieldNameIndex(tmp, "field2")
+	sdx, edx := ExtractNoValueFieldNameIndex(tmp, "field2")
 	t.Log(sdx, edx)
-	require.Equal(t, "field2", tmp[sdx:edx])
+	require.Equal(t, "field2 ", tmp[sdx:edx])
 }
 
-func TestExtractNoValueTagFieldNameIndex_7(t *testing.T) {
-	const tmp = "column:name;index;field2;"
+func TestExtractFieldEqualsValueIndex(t *testing.T) {
+	const tmp = "column:name;index:idx_myname;field2:value"
 
-	sdx, edx := ExtractNoValueTagFieldNameIndex(tmp, "field2")
+	sdx, edx := ExtractFieldEqualsValueIndex(tmp, "index", "idx_myname")
 	t.Log(sdx, edx)
-	require.Equal(t, "field2", tmp[sdx:edx])
+	require.Equal(t, "idx_myname", tmp[sdx:edx])
 }
 
-func TestExtractNoValueTagFieldNameIndex_8(t *testing.T) {
-	const tmp = "column:name;index;field2:value;"
+func TestExtractFieldEqualsValueIndex_2(t *testing.T) {
+	const tmp = "column:name;index:idx_myname ;field2:value"
 
-	sdx, edx := ExtractNoValueTagFieldNameIndex(tmp, "field2")
-	require.Equal(t, -1, sdx)
-	require.Equal(t, -1, edx)
+	sdx, edx := ExtractFieldEqualsValueIndex(tmp, "index", "idx_myname")
+	t.Log(sdx, edx)
+	require.Equal(t, "idx_myname ", tmp[sdx:edx])
 }
 
-func TestExtractNoValueTagFieldNameIndex_9(t *testing.T) {
-	const tmp = "column:name;index;field2: value;"
+func TestExtractFieldEqualsValueIndex_3(t *testing.T) {
+	const tmp = "column:name;index: idx_myname;field2:value"
 
-	sdx, edx := ExtractNoValueTagFieldNameIndex(tmp, "field2")
-	require.Equal(t, -1, sdx)
-	require.Equal(t, -1, edx)
+	sdx, edx := ExtractFieldEqualsValueIndex(tmp, "index", "idx_myname")
+	t.Log(sdx, edx)
+	require.Equal(t, " idx_myname", tmp[sdx:edx])
 }
 
-func TestExtractNoValueTagFieldNameIndex_10(t *testing.T) {
-	const tmp = "column:name;index :value;field2"
+func TestExtractFieldEqualsValueIndex_4(t *testing.T) {
+	const tmp = "column:name;index: idx_myname;index: idx_myname2;"
 
-	sdx, edx := ExtractNoValueTagFieldNameIndex(tmp, "index")
-	require.Equal(t, -1, sdx)
-	require.Equal(t, -1, edx)
+	sdx, edx := ExtractFieldEqualsValueIndex(tmp, "index", "idx_myname2")
+	t.Log(sdx, edx)
+	require.Equal(t, " idx_myname2", tmp[sdx:edx])
+}
+
+func TestExtractFieldEqualsValueIndex_5(t *testing.T) {
+	const tmp = "column:name;index: idx_myname2;index: idx_myname;"
+
+	sdx, edx := ExtractFieldEqualsValueIndex(tmp, "index", "idx_myname")
+	t.Log(sdx, edx)
+	require.Equal(t, " idx_myname", tmp[sdx:edx])
+}
+
+func TestExtractFieldEqualsValueIndexV2(t *testing.T) {
+	const tmp = "column:name;index: idx_myname;index: idx_myname2,priority:2;"
+	//不仅以分号或结尾为分割，还以逗号为分隔符
+	sdx, edx := ExtractFieldEqualsValueIndexV2(tmp, "index", "idx_myname2", []string{","})
+	t.Log(sdx, edx)
+	require.Equal(t, " idx_myname2", tmp[sdx:edx])
 }
